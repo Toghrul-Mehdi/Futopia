3ea1c9eedf4d03113868678f5cfbb53015af4a7d
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-03 00:57:35 +0400

    Add IFirebaseSmsService interface for OTP handling
    Introduced a new namespace `Futopia.UserService.Application.Abstractions.Third_Party` and added the `IFirebaseSmsService` interface.
    
    The interface includes two asynchronous methods:
    - `VerifyOtpCodeAsync`: Verifies an OTP code for a phone number and verification ID.
    - `SendOtpCodeAsync`: Sends an OTP code to a specified phone number.
    
    These methods return a `Response` object and are designed for OTP-related operations.

---
+++ src/Futopia.UserService/Futopia.UserService.Core/Futopia.UserService.Application/Abstractions/Third-Party/IFirebaseSmsService.cs
@@ -0,0 +1,7 @@
+﻿namespace Futopia.UserService.Application.Abstractions.Third_Party;
+
+public interface IFirebaseSmsService
+{
+    Task<Response> VerifyOtpCodeAsync(string phoneNumber, string otpCode, string verificationId);
+    Task<Response> SendOtpCodeAsync(string phoneNumber);
+}
