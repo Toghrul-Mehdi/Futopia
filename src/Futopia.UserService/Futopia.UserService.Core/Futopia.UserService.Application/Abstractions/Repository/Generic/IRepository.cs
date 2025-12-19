c46fed88727c0e0a3c9c89be82e321aa75a24a97
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-19 20:15:35 +0400

    feat: Implement IUserRefreshTokenRepository - Connected to AppDbContext with proper queries - Added soft delete support and indexing

---
+++ src/Futopia.UserService/Futopia.UserService.Core/Futopia.UserService.Application/Abstractions/Repository/Generic/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Futopia.UserService.Domain.Entities.Common;
+using System.Linq.Expressions;
+namespace Futopia.UserService.Application.Abstractions.Repository.Generic;
+
+public interface IRepository<T> where T : BaseEntity, new()
+{
+    IQueryable<T> GetAll(
+        Expression<Func<T, bool>>? expression = null,
+        Expression<Func<T, object>>? orderExpression = null,
+        int skip = 0,
+        int take = 0,
+        bool isDescending = false,
+        bool isTracking = false,
+        bool ignoreQuery = false,
+        params string[]? includes);
+
+    Task<T> GetByIdAsync(string id, params string[] includes);
+    Task AddAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task<int> SaveChangesAsync();
+    Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+}
