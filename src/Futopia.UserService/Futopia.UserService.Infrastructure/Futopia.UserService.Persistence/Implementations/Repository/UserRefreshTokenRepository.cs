b86ff9e2aee7142b7838d0202a98e2dd401bc0b7
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-19 20:17:17 +0400

    feat: Add IUserRefreshTokenService interface - Defined methods for token storage and retrieval

---
+++ src/Futopia.UserService/Futopia.UserService.Infrastructure/Futopia.UserService.Persistence/Implementations/Repository/UserRefreshTokenRepository.cs
@@ -0,0 +1,24 @@
+﻿using Futopia.UserService.Application.Abstractions.Repository;
+using Futopia.UserService.Domain.Entities;
+using Futopia.UserService.Persistence.Context;
+using Futopia.UserService.Persistence.Implementations.Repository.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Futopia.UserService.Persistence.Implementations.Repository;
+
+public class UserRefreshTokenRepository : Repository<UserRefreshToken>, IUserRefreshTokenRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRefreshTokenRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserRefreshToken?> GetByTokenAsync(string refreshToken)
+    {
+        return await _context.UserRefreshTokens
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.RefreshToken == refreshToken);
+    }
+}
