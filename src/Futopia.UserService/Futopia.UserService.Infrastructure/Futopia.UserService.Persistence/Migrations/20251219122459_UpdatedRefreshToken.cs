e03f9fd828fcd4775fe3d45aa9e545913cfef372
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-19 20:17:52 +0400

    feat: Add migration 20251219122459_UpdateUserRefreshToken_Updated - Fixed migration conflicts and ordering - Ensured clean upgrade path

---
+++ src/Futopia.UserService/Futopia.UserService.Infrastructure/Futopia.UserService.Persistence/Migrations/20251219122459_UpdatedRefreshToken.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Futopia.UserService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserRefreshTokens",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "UserRefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserRefreshTokens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserRefreshTokens",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "UserRefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "UserRefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "UserRefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserRefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserRefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "UserRefreshTokens");
+        }
+    }
+}
