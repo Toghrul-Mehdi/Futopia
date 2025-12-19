a8b4b765eea239d998fb7b12f44e2ecae02ef661
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-19 20:15:20 +0400

    feat: Add IUserRefreshTokenRepository interface - Defined async methods for refresh token CRUD - Added GetByTokenAsync and GetByUserIdAsync

---
+++ src/Futopia.UserService/Futopia.UserService.Core/Futopia.UserService.Application/Abstractions/Repository/IUserRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using Futopia.UserService.Application.Abstractions.Repository.Generic;
+using Futopia.UserService.Domain.Entities;
+
+namespace Futopia.UserService.Application.Abstractions.Repository;
+
+public interface IUserRefreshTokenRepository : IRepository<UserRefreshToken>
+{
+    // Token ilə refresh token-i tapmaq
+    Task<UserRefreshToken?> GetByTokenAsync(string refreshToken);    
+}
