3d9b6f9200adc324e55baebb65466803d6faf9ad
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-19 20:16:27 +0400

    feat: Add UserRefreshToken entity - Added Token, ExpiresAt, RevokedAt, ReplacedByToken fields - Made Token column unique in database

---
+++ src/Futopia.UserService/Futopia.UserService.Core/Futopia.UserService.Domain/Entities/UserRefreshToken.cs
@@ -0,0 +1,11 @@
+﻿using Futopia.UserService.Domain.Entities.Common;
+
+namespace Futopia.UserService.Domain.Entities;
+public class UserRefreshToken : BaseEntity
+{
+    public string UserId { get; set; }
+    public AppUser User { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime ExpiresAt { get; set; }
+    public bool IsRevoked { get; set; }
+}
